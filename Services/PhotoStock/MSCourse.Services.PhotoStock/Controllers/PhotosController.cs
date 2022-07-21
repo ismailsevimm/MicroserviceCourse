a264a1d44a2c0a5c527ff25aa026de629b0caf5a
ismailsevimm <ismail.sevim@yandex.com>
2022-07-21 19:06:42 +0300

    Photo Stock Microservice added

---
+++ Services/PhotoStock/MSCourse.Services.PhotoStock/Controllers/PhotosController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MSCourse.Services.PhotoStock.Dtos;
+using MSCourse.Shared.ControllerBases;
+using MSCourse.Shared.Dtos;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MSCourse.Services.PhotoStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotosController : CustomControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> PhotoSave(IFormFile file, CancellationToken cancellationToken)
+        {
+            if (file == null && file.Length < 1)
+            {
+                return CreateActionResultInstance(Response<PhotoDto>.Fail("File is empty", 400));
+
+            }
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Photos", file.FileName);
+
+            using var stream = new FileStream(path, FileMode.Create);
+            await file.CopyToAsync(stream, cancellationToken);
+
+            PhotoDto photo = new() { Url = "Photos/" + file.FileName };
+
+            return CreateActionResultInstance(Response<PhotoDto>.Success(photo, 200));
+        }
+
+        [HttpDelete]
+        public IActionResult PhotoDelete(string photoUrl)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Photos", photoUrl);
+
+            if (!System.IO.File.Exists(path))
+            {
+                return CreateActionResultInstance(Response<NoContent>.Fail("Photo is not found", 404));
+            }
+
+            System.IO.File.Delete(path);
+
+            return CreateActionResultInstance(Response<NoContent>.Success(204));
+        }
+    }
+}
---
+++ Services/PhotoStock/MSCourse.Services.PhotoStock/Dtos/PhotoDto.cs
@@ -0,0 +1,7 @@
+﻿namespace MSCourse.Services.PhotoStock.Dtos
+{
+    public class PhotoDto
+    {
+        public string Url { get; set; }
+    }
+}
