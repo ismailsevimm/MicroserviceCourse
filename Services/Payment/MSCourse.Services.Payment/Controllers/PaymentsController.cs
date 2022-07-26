f0b77b0107f3210e42842e3dbf4e03084e1f7ca5
ismailsevimm <ismail.sevim@yandex.com>
2022-07-26 20:32:38 +0300

    Payment Microservice added

---
+++ Services/Payment/MSCourse.Services.Payment/Controllers/PaymentsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MSCourse.Shared.ControllerBases;
+using MSCourse.Shared.Dtos;
+using MSCourse.Shared.Services.Interfaces;
+
+namespace MSCourse.Services.Payment.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PaymentsController : CustomControllerBase
+    {
+        private readonly ISharedIdentityService _sharedIdentityService;
+
+        public PaymentsController(ISharedIdentityService sharedIdentityService)
+        {
+            _sharedIdentityService = sharedIdentityService;
+        }
+
+        [HttpPost]
+        public IActionResult ReceivePayment()
+        {
+            return CreateActionResultInstance(Response<string>.Success(_sharedIdentityService.GetUserId,200));
+        }
+    }
+}
---
+++ Services/Payment/MSCourse.Services.Payment/Startup.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using MSCourse.Shared.Services;
+using MSCourse.Shared.Services.Interfaces;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace MSCourse.Services.Payment
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+            {
+                opt.Authority = Configuration["IdentityServerURL"];
+                opt.Audience = "resource_payment";
+                opt.RequireHttpsMetadata = false;
+            });
+
+            services.AddHttpContextAccessor();
+            services.AddScoped<ISharedIdentityService, SharedIdentityService>();
+
+            services.AddControllers(
+                opt => {
+                    opt.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy));
+                });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MSCourse.Services.Payment", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MSCourse.Services.Payment v1"));
+            }
+
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
