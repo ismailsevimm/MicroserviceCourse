7c06ffaf87da3c12ddff13c19d90275c5c371b9e
ismailsevimm <ismail.sevim@yandex.com>
2022-07-28 17:05:55 +0300

    Frontend'e IIdentity Service eklendi. LaunchSettings'de uygulama portu düzenlendi.

---
+++ Frontend/MSCourse.Web/Models/SigninInput.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MSCourse.Web.Models
+{
+    public class SigninInput
+    {
+        [Display(Name = "E-Posta adresiniz")]
+        public string Email { get; set; }
+
+        [Display(Name = "Şifreniz")]
+        public string Password { get; set; }
+
+        [Display(Name = "Beni hatırla")]
+        public bool IsRemember { get; set; }
+    }
+}
---
+++ Frontend/MSCourse.Web/Services/Interfaces/IIdentityService.cs
@@ -0,0 +1,14 @@
+﻿using MSCourse.Shared.Dtos;
+using MSCourse.Web.Models;
+using System.Threading.Tasks;
+
+namespace MSCourse.Web.Services.Interfaces
+{
+    public interface IIdentityService
+    {
+        Task<Response<bool>> SignIn(SigninInput signinInput);
+        Task<TokenResponse> GetAccessTokenByRefreshToken();
+
+        Task RevokeRefreshToken();
+    }
+}
